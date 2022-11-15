dc80b8fe4997524ce635e97334d2cd2c59131380
JohnQHerman <johnqherman@gmail.com>
2022-11-15 13:12:30 -0500

    starting fresh

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var app = express();
-app.use('/', function (req, res) { return res.send("Hello World!"); });
-app.listen(3000, function () { return console.log("server started on port 3000. listening..."); });
-//# sourceMappingURL=index.js.map
