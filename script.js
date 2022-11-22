f8618eb3f127fa95f476760bb9735a01bd4af725
JohnQHerman <johnqherman@gmail.com>
2022-11-22 17:44:18 -0500

    11/22 work

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+
+
 var counter = 0;
 
-// increment counter by 1 when button is clicked
 function incrementCounter() {
     counter++;
     document.getElementById('counter').innerHTML = counter;
@@ -15,3 +16,19 @@ function resetCounter() {
     counter = 0;
     document.getElementById('counter').innerHTML = counter;
 }
+
+function getUserSync() {
+    fetch('https://randomuser.me/api')
+        .then(r => r.json())
+        .then(RandomUserDataJSON => {
+            document.getElementById('json').innerHTML = JSON.stringify(RandomUserDataJSON, null, 2);
+            document.getElementById('fullname').innerHTML = RandomUserDataJSON.results[0].name.first + " " + RandomUserDataJSON.results[0].name.last;
+            document.getElementById('email').innerHTML = RandomUserDataJSON.results[0].email;
+            document.getElementById('picture').src = RandomUserDataJSON.results[0].picture.large;
+            document.getElementById('age').innerHTML = "Age " + RandomUserDataJSON.results[0].dob.age;
+            document.getElementById('cell').innerHTML = RandomUserDataJSON.results[0].cell;
+            document.getElementById('address').innerHTML = RandomUserDataJSON.results[0].location.street.number + " " + RandomUserDataJSON.results[0].location.street.name + ", " + RandomUserDataJSON.results[0].location.city + ", " + RandomUserDataJSON.results[0].location.state + ", " + RandomUserDataJSON.results[0].location.country + " " + RandomUserDataJSON.results[0].location.postcode;
+        });
+}
+
+window.onload = getUserSync;
